8029b84b55530cc3cea6f63cbe43dbbc91aba720
satou <satou.tatsuya@idh-net.co.jp>
2018-07-27 00:04:04 +0900

    add : compornentを分ける

---
+++ src/components/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>title</v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  name: 'Header'
+}
+</script>
---
+++ src/pages/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app id="HelloWorld">
+    <myheader></myheader>
+    <v-container>
+      HelloWorld
+    </v-container>
+  </v-app>
+</template>
+<script>
+import Header from '@/components/header'
+export default {
+  name: 'HelloWorld',
+  components: {
+    'myheader': Header
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
